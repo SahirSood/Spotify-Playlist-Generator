f00d5dec5eb29ff4295aabf2939acdcbc3a9fafb
SahirSood <114258435+SahirSood@users.noreply.github.com>
2026-03-16 22:24:04 -0700

    Test update

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders spotify landing page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/spotify playlist generator/i);
+  expect(heading).toBeInTheDocument();
 });
